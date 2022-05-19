a26d9648df588922a49a2ad8b94b955477ebcaca
Brian Austin <austin.brian@gmail.com>
2022-05-19 16:19:00 -0400

    Update dashboard to be responsive and to have emoji too

--- dashboard.py
+++ dashboard.py
@@ -1,4 +1,5 @@
 import os
+import random
 
 import dash
 import flask
@@ -7,6 +8,7 @@ from dash import dcc, html
 from dash.dependencies import Input, Output
 
 import figures as figs
+from metrics import create_metric
 
 
 def create_dash_app(
@@ -32,31 +34,56 @@ def create_dash_app(
 
     app.layout = html.Div(
         [
+            html.H2(
+                id="topblocks",
+                children="\U0001F7E9 \U0001F7E8 \U0001F7E9 \U0001F7E8 \U00002B1C",
+            ),
             html.H1("WordleStats Dashboard"),
             dcc.Dropdown(
-                id="my-dropdown",
+                id="dropdown",
                 options=[
-                    {"label": "Tesla", "value": "TSLA"},
-                    {"label": "Apple", "value": "AAPL"},
-                    {"label": "Coke", "value": "COKE"},
+                    {"label": "Average number of attempts", "value": "avg-guesses"},
+                    {"label": "Number of games played", "value": "num-games"},
+                    {"label": "Number of wins", "value": "wins"},
+                    {"label": "Number of losses", "value": "losses"},
+                    {"label": "Percentage win", "value": "pwin"},
+                    {"label": "Attempts per win", "value": "apw"},
                 ],
-                value="TSLA",
+                value="num-games",
             ),
-            dcc.Graph(
-                id="number-games-played", figure=figs.number_of_games_bar_graph(df)
+            dcc.Graph(id="bar-graph", figure=figs.number_of_games_bar_graph(df)),
+            html.H2(
+                id="bottomblocks",
+                children="\U0001F7E9 \U0001F7E8 \U0001F7E9 \U0001F7E8 \U00002B1C",
             ),
         ],
         className="container",
     )
 
-    @app.callback(Output("my-graph", "figure"), [Input("my-dropdown", "value")])
-    def update_graph(selected_dropdown_value):
-        dff = df[df["Stock"] == selected_dropdown_value]
-        return {
-            "data": [
-                {"x": dff.Date, "y": dff.Close, "line": {"width": 3, "shape": "spline"}}
-            ],
-            "layout": {"margin": {"l": 30, "r": 20, "b": 30, "t": 20}},
-        }
+    @app.callback(
+        Output("bar-graph", "figure"),
+        [Input("dropdown", "value"), Input("dropdown", "label")],
+    )
+    def update_graph(selected_dropdown_value, selected_dropdown_label):
+        data = create_metric(df, selected_dropdown_value)
+        fig = figs.bar_graph_of_names(data, selected_dropdown_label)
+
+        return fig
+
+    @app.callback(Output("topblocks", "children"), [Input("dropdown", "value")])
+    def update_top_bar(value):
+        """
+        Update the wordle boxes at the top every time we change the dropdown val
+        """
+        boxes = ["\U0001F7E9", "\U0001F7E8", "\U00002B1C"]
+        return " ".join(random.choices(boxes, k=5))
+
+    @app.callback(Output("bottomblocks", "children"), [Input("dropdown", "value")])
+    def update_bottom_bar(value):
+        """
+        Update the wordle boxes at the bottom every time we change the dropdown val
+        """
+        boxes = ["\U0001F7E9", "\U0001F7E8", "\U00002B1C"]
+        return " ".join(random.choices(boxes, k=5))
 
     return app
